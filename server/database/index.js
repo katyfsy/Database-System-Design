e0d5821bd6a88b3459a3e87dacee950079d1bd74
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-04-27 13:51:01 -0400

    update db instance ip address

--- server/database/index.js
+++ server/database/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { USER, PASSWORD, DB_PORT } = process.env;
 
 const pool = new Pool({
-  host: 'ec2-34-201-112-63.compute-1.amazonaws.com',
+  host: 'ec2-100-25-217-146.compute-1.amazonaws.com',
   user: USER,
   password: PASSWORD,
   port: DB_PORT,
@@ -18,11 +18,3 @@ pool
   .catch((err) => console.error(err));
 
 module.exports = pool;
-
-// const queryString = 'SELECT * from questions LEFT JOIN answers on questions.id = answers.question_id LEFT JOIN answers_photos ON answers.id = answers_photos.answer_id WHERE questions.product_id = $1 ORDER BY questions.helpful DESC;';
-// const values = [37312];
-
-// pool
-//   .query(queryString, values)
-//   .then((res) => console.log(res.rows))
-//   .catch((err) => console.error('Error executing query', err.stack));
